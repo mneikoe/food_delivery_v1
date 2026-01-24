ab73a12005a6f72720591694c6d80ff0130bd6bb
AMAN RAJ <raj117557@gmail.com>
2026-01-24 21:50:12 +0530

    dddeefsdfdsadfs

--- server/src/utils/otpGenerator.js
+++ server/src/utils/otpGenerator.js
@@ -1,6 +1,11 @@
-const generateOTP = () => {
-  // Generate 6-digit OTP (100000 to 999999)
-  return Math.floor(100000 + Math.random() * 900000).toString();
+const generateOTP = (length = 4) => {
+  // Default: 4-digit OTP for delivery (1000 to 9999)
+  // Can be used with length parameter: generateOTP(6) for auth OTP
+  if (length === 6) {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+  }
+  // 4-digit OTP for delivery
+  return Math.floor(1000 + Math.random() * 9000).toString();
 };
 
 module.exports = { generateOTP };
