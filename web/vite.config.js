bf08e625b3555ef4419612558308a5298193af0f
Aman Raj <raj117557@gmail.com>
2026-07-08 16:35:43 +0530

    8th July

--- web/vite.config.js
+++ web/vite.config.js
@@ -8,12 +8,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://www.chatoraadda.in',
         changeOrigin: true,
         rewrite: (path) => path,
       },
       '/uploads': {
-        target: 'http://localhost:8080',
+        target: 'https://www.chatoraadda.in',
         changeOrigin: true,
         rewrite: (path) => path,
       },
